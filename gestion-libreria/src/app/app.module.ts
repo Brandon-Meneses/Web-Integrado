ccc3b262fa8315c03801d65244868cedfb25cfb2
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-04-22 12:07:56 -0500

    Añadir componentes Navbar, BookSearch y Dashboard

--- gestion-libreria/src/app/app.module.ts
+++ gestion-libreria/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BookSearchComponent } from './components/book-search/book-search.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BookSearchComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
