5eb812cbf6ef4567fe3d06c045c024faeb96b841
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-04-24 17:25:18 -0500

    Agrego implementación Serializable a las clases LibroEntity, StockSucursalEntity y SucursalEntity

--- src/main/java/com/utp/webintegrado/persistence/entity/LibroEntity.java
+++ src/main/java/com/utp/webintegrado/persistence/entity/LibroEntity.java
@@ -1,5 +1,7 @@
 package com.utp.webintegrado.persistence.entity;
 
+import java.io.Serializable;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(name = "libro")
-public class LibroEntity {
+public class LibroEntity implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
