5f322cc9b53294100be7c27292a248d6fc90d3e2
Leonardo Espejo <85245534+leo-spj@users.noreply.github.com>
2024-05-03 18:44:02 -0500

    Actualizo anotaciones de persistencia en SucursalEntity y SucursalService

--- src/main/java/com/utp/webintegrado/persistence/entity/SucursalEntity.java
+++ src/main/java/com/utp/webintegrado/persistence/entity/SucursalEntity.java
@@ -1,10 +1,7 @@
 package com.utp.webintegrado.persistence.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,7 +12,8 @@ import lombok.Setter;
 public class SucursalEntity {
 
     @Id
-    @Column(name = "id_sucursal")
+    @Column(name = "id_sucursal", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int idSucursal;
 
     @Column(name = "nombre", nullable = false)
