816ca4fc3e30907b4384263a302fe4ce988136cf
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-05-22 21:38:34 -0500

    Interfaz de contacto lista

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { HomeComponent } from './components/home/home.component';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { ReportsComponent } from './components/reports/reports.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  { path: 'inicio', component: InicioComponent }
-  
+  { path: '', component: InicioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'inventory', component: InventoryComponent },
+  { path: 'reports', component: ReportsComponent }
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
