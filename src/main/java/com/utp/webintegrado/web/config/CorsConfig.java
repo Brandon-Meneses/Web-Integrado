7baa2e80871a52778404e4aad2e3f63393a51318
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-05-23 22:59:04 -0500

    seguridad: CORS, permite solicitudes desde localhost:4200. Se han permitido los métodos GET, POST, PUT y DELETE.

---
+++ src/main/java/com/utp/webintegrado/web/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.utp.webintegrado.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+}
