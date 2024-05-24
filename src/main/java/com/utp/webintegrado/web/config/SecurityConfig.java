3d7e22504118e67f1f8cfb965c428a1b8967d4b9
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-05-24 00:23:13 -0500

    usuario admin admin para autenticacion. y passw encoder

--- src/main/java/com/utp/webintegrado/web/config/SecurityConfig.java
+++ src/main/java/com/utp/webintegrado/web/config/SecurityConfig.java
@@ -2,7 +2,14 @@ package com.utp.webintegrado.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,10 +21,26 @@ public class SecurityConfig {
                 .csrf().disable() // desactiva la protección antiafalsificación de solicitudes csrf (no para producción)
                 .cors().and() // habilita la configuración de cors, por defecto no permite que otros dominios accedan a la aplicación
                 .authorizeHttpRequests()
-                .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll() // permite todas las solicitudes a estas rutas
+                .requestMatchers(HttpMethod.GET, "/api/**").permitAll() // permite todas las solicitudes GET a /api/**
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll() // permite todas las solicitudes a estas rutas
                 .anyRequest().authenticated() // cualquier otra solicitud debe estar autenticada
                 .and()
                 .httpBasic(); // debe ser autenticado con http basic
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
