be2ed73b10d5aee7899eb2b93c183dd1b98a5298
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-05-29 12:34:32 -0500

    servlet de testeo

--- src/main/java/com/utp/webintegrado/web/servlets/CustomServlet.java
+++ src/main/java/com/utp/webintegrado/web/servlets/CustomServlet.java
@@ -1,5 +1,6 @@
 package com.utp.webintegrado.web.servlets;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,4 +12,19 @@ public class CustomServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.getWriter().write("Respuesta desde el servlet personalizado");
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String titulo = req.getParameter("titulo");
+
+        if (titulo == null || titulo.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().write("El título del libro no puede estar vacío");
+            return;
+        }
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().write("El título del libro es: " + titulo);
+    }
+
 }
